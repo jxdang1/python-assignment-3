adf9128823dd16e2aabc374c4da02429755d423d
Jennifer Dang <144502402+jxdang1@users.noreply.github.com>
2025-08-27 12:04:49 -0500

    defining functions and arrays

--- Assignment_3.py
+++ Assignment_3.py
@@ -5,6 +5,19 @@ print("* Jennifer Dang                            *")
 print("* IS 826: Application Programming          *")
 print("* Assignment 3: Data Structure & Algorithm *")
 print("* Date: 08/27/2025                         *")
-print("******************************************\n")
+print("********************************************\n")
 
 print("""Purpose: \n""")
+
+# Prompt the user to enter a string that returns the input
+
+def getInput():
+    input_user = input("Enter a string to evaluate:")
+    return input_user
+
+def characterCount(input_string):
+    count = 0
+    for char in input_string:
+        count +=1
+    return count
+
