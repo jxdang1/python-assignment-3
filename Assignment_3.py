bcd1c70daa6db5382f0cc355729c2627d92e68d6
Jennifer Dang <144502402+jxdang1@users.noreply.github.com>
2025-08-27 11:40:13 -0500

    Add files via upload
    added header and purpose

---
+++ Assignment_3.py
@@ -0,0 +1,10 @@
+# Header
+
+print("********************************************")
+print("* Jennifer Dang                            *")
+print("* IS 826: Application Programming          *")
+print("* Assignment 3: Data Structure & Algorithm *")
+print("* Date: 08/27/2025                         *")
+print("******************************************\n")
+
+print("""Purpose: \n""")
